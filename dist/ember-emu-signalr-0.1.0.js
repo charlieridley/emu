2dbf1c1d19df3c53ab5d306eff22f82009a61dc2
charlieridley <charlieridley@gmail.com>
2013-04-09 10:34:31 -0400

    updated dist

--- dist/ember-emu-signalr-0.1.0.js
+++ dist/ember-emu-signalr-0.1.0.js
@@ -1,12 +1,12 @@
 (function() {
   Emu.SignalrPushDataAdapter = Emu.PushDataAdapter.extend({
     registerForUpdates: function(store, type) {
-      var hub, modelKey, _ref,
+      var hub, modelKey, _ref, _ref1,
         _this = this;
 
       modelKey = this._serializer.serializeTypeName(type);
       if (hub = (_ref = $.connection) != null ? _ref[modelKey + "Hub"] : void 0) {
-        return hub.updated = function(json) {
+        return (_ref1 = hub.updated) != null ? _ref1 : hub.updated = function(json) {
           return _this.didUpdate(type, store, json);
         };
       }
